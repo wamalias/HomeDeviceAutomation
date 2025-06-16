11ef11f04bb5c54dfa3525d95d26369e1c53ff5c
gabriellanatasya <gabriellanatasya28@gmail.com>
2025-06-16 09:55:06 +0700

    Add swift app template

---
+++ HomeDeviceAutomation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomeDeviceAutomation
+//
+//  Created by Gabriella Natasya Pingky Davis on 16/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomeDeviceAutomation/HomeDeviceAutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeDeviceAutomationApp.swift
+//  HomeDeviceAutomation
+//
+//  Created by Gabriella Natasya Pingky Davis on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeDeviceAutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
