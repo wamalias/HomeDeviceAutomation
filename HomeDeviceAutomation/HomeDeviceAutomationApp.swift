0142c5b6ecbfb1f85da55d33e8c2e2bc822599a9
wamalias <wardamalias@gmail.com>
2025-06-16 11:52:37 +0700

    Initial Commit

---
+++ HomeDeviceAutomation/HomeDeviceAutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeDeviceAutomationApp.swift
+//  HomeDeviceAutomation
+//
+//  Created by Wardatul Amalia Safitri on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeDeviceAutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeDeviceAutomationTests/HomeDeviceAutomationTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeDeviceAutomationTests.swift
+//  HomeDeviceAutomationTests
+//
+//  Created by Wardatul Amalia Safitri on 16/06/25.
+//
+
+import Testing
+@testable import HomeDeviceAutomation
+
+struct HomeDeviceAutomationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
